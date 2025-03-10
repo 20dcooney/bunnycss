5c01b540c5e5377886f45a35183f2644a197088c
20dcooney <davidjcooney33@gmail.com>
2025-03-10 00:59:37 -0400

    javascript changes allow bunny-one to act as a class and be used multiple times, and allows the SVG to take different colors

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ document.addEventListener('DOMContentLoaded', function () {
     fetch('./svg/bunny-one.svg')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('bunny-three').innerHTML = data;
+            const svgContainer = new DOMParser().parseFromString(data, 'image/svg+xml');
+
+            const paths = svgContainer.querySelectorAll('path');
+            paths.forEach(path => {
+                path.classList.add('colorhandling');
+            });
+
+            const svgElements = document.querySelectorAll('.bunny-one');
+
+            svgElements.forEach(element => {
+                element.innerHTML = '';
+                const svgElement = svgContainer.querySelector('svg').cloneNode(true);
+                element.appendChild(svgElement);
+            });
         })
         .catch(error => console.error('Error loading SVG:', error));
 });
